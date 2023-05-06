bb17e08647b640c7a36571d3b14d52a343e8832c
Gary Namestnik <gazzenger@gmail.com>
2023-05-06 11:38:23 +0800

    Fix up action buttons

--- src/components/Shortcuts.js
+++ src/components/Shortcuts.js
@@ -3,18 +3,24 @@ import { Component } from 'preact';
 import { html } from '../html.js'
 
 export class Shortcuts extends Component {
+
+  emulateKeyPress(key) {
+    document.dispatchEvent(new KeyboardEvent('keydown', {key}));
+    document.getElementById('shortcuts').focus();
+  }
+
   render () {
     return html`
       <div id="shortcuts">
         <div>
-          <p><span>'space'</span> = show/hide details</p><button aria-label="Hide page details">Hide Details</button>
+          <p><span>'space'</span> = show/hide details</p><button onclick="${() => this.emulateKeyPress(' ')}" aria-label="Hide page details">Hide Details</button>
         </div>
         <div>
-          <p><span>'enter'</span> = stop/start color pattern</p><button aria-label="Stop the color pattern">Stop Color Pattern</button>
+          <p><span>'enter'</span> = stop/start color pattern</p><button onclick="${() => this.emulateKeyPress('Enter')}" aria-label="Stop the color pattern">Stop Color Pattern</button>
         </div>
           ${
             document.fullscreenEnabled
-              ? html`<p><span>'f'</span> = enter/leave fullscreen</p><button aria-label="Enter fullscreen mode">Enter Fullscreen</button>`
+              ? html`<p><span>'f'</span> = enter/leave fullscreen</p><button  onclick="${() => this.emulateKeyPress('f')}" aria-label="Enter fullscreen mode">Enter Fullscreen</button>`
               : ''
           }
       </div>
